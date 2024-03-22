c78ee83df00f926208cd891d96b6dd75a3a60164
shivamvishwakarma02 <shivam.vishwakarma02>
2024-03-22 05:56:40 +0530

    initial commit

---
+++ src/main/java/com/pipeline/PipelineApplication.java
@@ -0,0 +1,15 @@
+package com.pipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipelineApplication {
+
+	public static void main(String[] args) 
+	{
+		SpringApplication.run(PipelineApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
