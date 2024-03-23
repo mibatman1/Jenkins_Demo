d6c6b51e170f7383d623f005955bd57d2aea9a7f
shivamvishwakarma02 <shivam.vishwakarma02>
2024-03-23 18:38:04 +0530

    add auto shutdown

--- src/main/java/com/pipeline/PipelineApplication.java
+++ src/main/java/com/pipeline/PipelineApplication.java
@@ -9,7 +9,15 @@ public class PipelineApplication {
 	public static void main(String[] args) 
 	{
 		SpringApplication.run(PipelineApplication.class, args);
-		System.out.println("Hello");
+		new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        System.exit(0); 
+                    }
+                },
+                20000 
+        );
 	}
 
 }
